45599507ae2eaf9639115c234af9c68424520846
mossi <mossi.hashemi@gmail.com>
2019-02-05 13:50:23 +0100

    add flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +15,10 @@ import { NavbarComponent } from './navbar/navbar.component';
     ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
     ],
   providers: [],
   bootstrap: [AppComponent]
