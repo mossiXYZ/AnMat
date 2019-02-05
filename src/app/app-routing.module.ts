b3fc28d09d89d925da95222b5e955aacbeac9d36
mossi <mossi.hashemi@gmail.com>
2019-02-05 15:30:31 +0100

    add some components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { HomeComponent }      from './home/home.component';
+import { LoginComponent }      from './login/login.component';
+import { ManageSeriesComponent } from './admin/manage-series/manage-series.component';
+import { ManageUsersComponent } from './admin/manage-users/manage-users.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'admin/series', component: ManageSeriesComponent },
+  { path: 'admin/users', component: ManageUsersComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,19 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment.prod';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ManageSeriesComponent } from './admin/manage-series/manage-series.component';
+import { ManageUsersComponent } from './admin/manage-users/manage-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent,
+    ManageSeriesComponent,
+    ManageUsersComponent
     ],
   imports: [
     BrowserModule,
