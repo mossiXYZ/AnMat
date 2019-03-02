354932bb74cfe3a3df5305d27a45f780cb50d34c
mossi <mossi.hashemi@gmail.com>
2019-03-02 12:00:29 +0100

    add Series to Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { SerieService } from '../shared/serie.service';
+
+
+interface Serie {
+  title: string,
+  year: string,
+  summary: string,
+  genre: string, 
+  imageUrl: string
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +16,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  allSeries : any[];
+  newSeries: any[];
+  recommandations: any[];
+  constructor(private serieService : SerieService) { }
 
   ngOnInit() {
+    this.serieService.getSeries().subscribe(
+       
+      listOfSeries => { 
+        let array  = listOfSeries.map(item => {
+          return {
+            $key : item.key,
+            ...item.payload.val()
+          };
+        });
+        //console.log(array);
+        this.allSeries = array;
+        this.newSeries = array.filter(item => item.rate >4);
+        this.recommandations = array.filter(item => item.dateread  == null);
+      });
+         
+          //console.log(array);
+
+
   }
 
 }
